69495f358a96f15db66e90917490d07877fc1b7e
Brandon Williams <brandonwilliams.88@yahoo.com>
2023-09-02 11:03:58 -0500

    input the prompt of questions into index.js, still working on file

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer');
+
+async function mainMenu() {
+    const answer = await inquirer.createPromptModule([
+        {
+            type: 'list',
+            name: 'menuChoice',
+            message: 'What would you like to do?',
+            choices: [
+             'View all departments',
+             'View all roles',
+             'View all employees',
+             'Add a department',
+             'Add a role',
+             'Add an employee',
+             'Update an employee role',
+             'Exit'
+           ],
+        },
+    ]);
+
+}
